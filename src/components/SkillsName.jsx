ca6193be6b61863e19e1cf555e3859a0c04c4815
harshupanghal <harshu7850@gmail.com>
2025-03-01 13:47:19 +0530

    changes made

--- src/components/SkillsName.jsx
+++ src/components/SkillsName.jsx
@@ -146,7 +146,7 @@ const blockchain = [
       <img
         src="/icons/hardhat.svg"
         alt="Hardhat"
-        className="max-w-full max-h-full scale-[2.5] object-contain"
+        className="max-w-full max-h-full scale-110 object-contain"
       />
     ),
   },
@@ -202,9 +202,9 @@ const blockchain = [
     name: "Metamask",
     icon: (
       <img
-        src="/icons/metamask.svg"
+        src="/icons/metamask.png"
         alt="Metamask"
-        className="max-w-full max-h-full scale-[3] object-contain"
+        className="max-w-full max-h-full object-contain"
       />
     ),
   },
@@ -280,7 +280,7 @@ const devTools = [
       <img
         src="/icons/git.png"
         alt="Git"
-        className="max-w-full max-h-full scale-150 object-contain"
+        className="max-w-full max-h-full scale-110 object-contain"
       />
     ),
   },
@@ -290,7 +290,7 @@ const devTools = [
       <img
         src="/icons/github.png"
         alt="Github"
-        className="max-w-full max-h-full object-contain"
+        className="max-w-full max-h-full scale-110 object-contain"
       />
     ),
   },
@@ -301,7 +301,7 @@ const devTools = [
       <img
         src="/icons/firebase.svg"
         alt="Firebase"
-        className="max-w-full max-h-full scale-[2] object-contain"
+        className="max-w-full max-h-full  object-contain"
       />
     ),
   },
@@ -325,7 +325,7 @@ const devTools = [
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
         aria-label="Render"
-        className=" scale-[1.5] pl-3 fill-current text-black dark:text-white"
+        className=" scale-[1.75] pt-4 pl-4  fill-current text-black dark:text-white"
       >
         <path d="M15.6491 0.00582604C12.9679 -0.120371 10.7133 1.81847 10.3286 4.373C10.3134 4.49154 10.2905 4.60627 10.2715 4.72099C9.67356 7.90268 6.88955 10.3119 3.5457 10.3119C2.35364 10.3119 1.23395 10.006 0.258977 9.47058C0.140914 9.40557 0 9.4897 0 9.62354V10.3081V20.6218H10.2677V12.8894C10.2677 11.4668 11.4178 10.3119 12.8346 10.3119H15.4015C18.3074 10.3119 20.6458 7.89121 20.5315 4.94662C20.4287 2.29649 18.2884 0.132023 15.6491 0.00582604Z"></path>
       </svg>
@@ -338,7 +338,7 @@ const devTools = [
       <img
         src="/icons/vercel.png"
         alt="Vercel"
-        className="max-w-full max-h-full scale-[2] object-contain"
+        className="max-w-full max-h-full scale-110 object-contain"
       />
     ),
   },
@@ -349,7 +349,7 @@ const devTools = [
       <img
         src="/icons/google.svg"
         alt="Google OAuth2"
-        className="max-w-full max-h-full scale-75 object-contain"
+        className="max-w-full max-h-full scale-50 object-contain"
       />
     ),
   },
